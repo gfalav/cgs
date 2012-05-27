201b62087f00d7aad1d2770f5e5814742f3d79b3
Gustavo <gfalav@hotmail.com>
2012-05-27 16:10:58 -0300

    lista portada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cgs::Application.config.secret_token = '3063c7cb65c6f1e2092c32874ee5dce1a037a56cc5f6fb3144a4d876ef06417da7f05e24c9dd34a083169590aa51e306eaf4d2b05dfc9ded81dbb528a53ce972'
