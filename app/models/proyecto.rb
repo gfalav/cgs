81b5e3fb43968a33994ebdc2e8ee43852e2cd57d
Gustavo <gfalav@hotmail.com>
2012-05-28 07:37:28 -0300

    empieza a mostrar mapa

--- app/models/proyecto.rb
+++ app/models/proyecto.rb
@@ -4,5 +4,6 @@ class Proyecto < ActiveRecord::Base
   belongs_to :zona
   belongs_to :cond_e, :class_name=>'Conductor', :foreign_key=>'cond_e_id'
   belongs_to :cond_g, :class_name=>'Conductor', :foreign_key=>'cond_g_id'
+  has_many :puntos
   
 end
--- app/models/punto.rb
+++ app/models/punto.rb
@@ -1,3 +1,5 @@
 class Punto < ActiveRecord::Base
   attr_accessible :angulo, :distancia, :latitud, :longitud, :proyecto_id, :punto, :secuencia
+  
+  belongs_to :proyecto
 end
